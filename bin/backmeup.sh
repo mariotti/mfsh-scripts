ca722a0d5af4efdea6a92b36b814df7b320a3c36
Fabio <fab.mariotti@gmail.com>
2016-12-25 22:42:13 +0100

    minor updates to backmeup

--- bin/backmeup.sh
+++ bin/backmeup.sh
@@ -8,16 +8,20 @@ DIRBACKUPS="${DIRRSYNC}-BP"
 mydate=`date +%Y%m%d-%H%M%S`   #FORMAT: 20130131-222618
 OPTRSYNC="-av --delete --backup"
 #
+# Parsing the one option
 if [ -z $1 ]; then
     echo "please give a dir name."
     exit 1;
 fi;
+#Remove trailing / it creates a project directory
+TOBACKUP=`dirname $1`/`basename $1`
 PRJDIR=`basename ${1}`
-DIRBKPUP=" --backup-dir=${DIRBACKUPS}/${PRJDIR}/B-${mydate}"
+DIRBKUP="${DIRBACKUPS}/${PRJDIR}/B-${mydate}"
+OPTBKUP=" --backup-dir=${DIRBKUP}"
 PRGDIRBACKUPS="${HOME}/tmp/rs/rsyncBackup-BP/${1}/"
-#rsync --dry-run ${OPTRSYNC} ${DIRBKPUP} $1 ${DIRRSYNC}/${PRJDIR}
-echo "Running: " rsync ${OPTRSYNC} ${DIRBKPUP} $1 ${DIRRSYNC}/${PRJDIR}
-rsync ${OPTRSYNC} ${DIRBKPUP} $1 ${DIRRSYNC}/${PRJDIR}
+#rsync --dry-run ${OPTRSYNC} ${OPTBKUP} $1 ${DIRRSYNC}/${PRJDIR}
+#echo "Running: " rsync ${OPTRSYNC} ${OPTBKUP} $1 ${DIRRSYNC}/${PRJDIR}
+rsync ${OPTRSYNC} ${OPTBKUP} ${TOBACKUP} ${DIRRSYNC}/${PRJDIR}
 #
 #
 # Create List Files
