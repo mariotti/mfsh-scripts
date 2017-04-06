d192a969b0411051851359c476e337b56bcdd17e
Fabio <fab.mariotti@gmail.com>
2017-04-06 22:59:20 +0200

    introducing and testing travis ci

---
+++ equality_test.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+# See: http://stackoverflow.com/questions/20449707/using-travis-ci-for-testing-on-unix-shell-scripts
+
+testEquality() {
+	assertEquals 1 1
+}
+
+. shunit2-2.1.6/src/shunit2
+Contact GitHub API Training Shop Blog About
+
